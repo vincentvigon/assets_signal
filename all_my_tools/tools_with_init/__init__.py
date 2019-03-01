c3dca39eeac8ef8d83cd1305360ef22d0ebd9857
vincentvigon <vincent.vigon@gmail.com>
2019-03-01 11:51:58 +0100

    tools

--- all_my_tools/tools_with_init/__init__.py
+++ all_my_tools/tools_with_init/__init__.py
@@ -1,5 +1,6 @@
-import myRandom.MyRandom
+import assets_signal.all_my_tools.tools_with_init.myRandom.MyRandom
 
 my_random=MyRandom()
+
 
 print("le fichier __init__ du package tools_with_init a été exécuté")
